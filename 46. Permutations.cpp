c5e511113b705a4278dccde1051ae1bff06aba4c
yiheng666 <yiheng.gml@gmail.com>
2021-06-28 23:05:13 +0900

    no message

---
+++ 46. Permutations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> track;
+        backtrack(nums,track);
+        return res;
+    }
+    void backtrack(vector<int> nums,vector<int> &track){
+        if (nums.size() == track.size()){
+            res.push_back(track);
+        }
+        for (int i=0;i<nums.size();i++){
+            if ( find(track.begin(), track.end(), nums[i]) != track.end() )
+                continue;
+            track.push_back(nums[i]);
+            backtrack(nums,track);
+            track.pop_back();
+        }
+    }
+};
