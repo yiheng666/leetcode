aef9d287571c6b1ae2df411a24901f0fb24d75ef
yiheng666 <yiheng.gml@gmail.com>
2021-07-05 20:58:55 +0900

    no message

--- model.cpp
+++ model.cpp
@@ -1,7 +1,26 @@
-void PreOrder(Bitree T){
-	if (T){
-		visit(T);
-		PreOrder(T->left);
-		PreOrder(T->right);
+//PreOrder
+void preorderTraversal(TreeNode* root){
+	if (root){
+		visit(root);
+		PreOrder(root->left);
+		PreOrder(root->right);
 	}
 }
+//PreOrder
+void preorderTraversal(TreeNode* root){
+	stack<TreeNode*> st;
+	auto p = root;
+	while (p || st.empty()){
+		if (p){
+			st.push(p);
+			visit(p->val);
+			p = p->left;
+		}
+		else{
+			auto node = st.top();
+			st.pop();
+			p = node->right;
+		}
+	}
+}
+
