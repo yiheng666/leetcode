d8d75168be83fc9fc853c455a6f7550b1db915c0
yiheng666 <yiheng.gml@gmail.com>
2021-07-12 20:49:21 +0900

    no message

---
+++ 213. House Robber II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+        if (nums.size() == 2) return max(nums[0],nums[1]);
+        int n = nums.size();
+        int ans = max(robFac(nums,0,n-2),robFac(nums,1,n-1));
+        return ans;
+    }
+    int robFac(vector<int> nums,int start,int end){
+        int n = nums.size();
+        vector<int> dp(n,0);
+        dp[start] = nums[start];
+        dp[start+1] = max(nums[start],nums[start+1]);
+        for (int i=start+2;i<=end;i++){
+            dp[i] = max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[end];
+    }
+};
