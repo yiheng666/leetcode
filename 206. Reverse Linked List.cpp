afaa8e615709fefed9df27b354036bb742952248
yiheng666 <yiheng.gml@gmail.com>
2021-07-18 01:21:56 +0900

    no message

--- 206. Reverse Linked List.cpp
+++ 206. Reverse Linked List.cpp
@@ -8,12 +8,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //1
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* ans = new ListNode(0),*cur = head;
         ans->next = head;
         ListNode* pre = ans;
+		//cur==NULL，即链表为空不做处理
+		//cur->next==NULL，反转结束
         while (cur && cur->next){
             ListNode* nex = cur->next;
             cur->next = nex->next;
@@ -21,5 +24,19 @@ public:
             pre->next = nex;
         }
         return ans->next;
+    }
+};
+ //2
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre=nullptr,*cur = head;
+        while (cur){
+            ListNode* nex = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = nex;
+        }
+        return pre;
     }
 };
