492a3b80183dc641206064497a3b8d6df5de7814
yiheng666 <yiheng.gml@gmail.com>
2021-08-10 21:21:00 +0900

    no message

--- model.cpp
+++ model.cpp
@@ -141,3 +141,40 @@ int right_bound(int[] nums, int target) {
         return -1;
     return right;
 }
+
+//根据前序中序生成树
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+	return build(preorder,0,preorder.size()-1,inorder,0,inorder.size()-1);
+}
+TreeNode* build(vector<int>& preorder,int preLeft,int preRight,vector<int>& inorder,int inLeft,int inRight){
+	if (preLeft > preRight || inLeft > inRight) return nullptr;
+	int val = preorder[preLeft],index = -1;
+	for (int i=inLeft;i<=inRight;i++){
+		if (val == inorder[i]){
+			index = i;
+			break;
+		}
+	}
+	TreeNode* root = new TreeNode(val);
+	root->left = build(preorder,preLeft+1,preLeft+index-inLeft,inorder,inLeft,index-1);
+	root->right = build(preorder,preLeft+index-inLeft+1,preRight,inorder,index+1,inRight);
+	return root;
+}
+
+//根据中序后序生成树
+TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+	return build(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1);
+}
+TreeNode* build(vector<int>& inorder,int inLeft,int inRight,vector<int>& postorder,int postLeft,int postRight){
+	if (inLeft > inRight || postLeft > postRight) return nullptr;
+	int val = postorder[postRight],index= -1;
+	for (int i=inLeft;i<=inRight;i++){
+		if (val == inorder[i]){
+			index = i;
+			break;
+		}
+	}
+	TreeNode* root = new TreeNode(val);
+	root->left = build(inorder,inLeft,index-1,postorder,postLeft,postLeft+index-inLeft-1);
+	root->right = build(inorder,index+1,inRight,postorder,postLeft+index-inLeft,postRight-1);
+	return root;
