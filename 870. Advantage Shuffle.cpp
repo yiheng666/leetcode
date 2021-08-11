80bc8fc0c1bf8d27e3334a7dc9deeb32c037128d
yiheng666 <yiheng.gml@gmail.com>
2021-08-11 23:58:18 +0900

    no message

--- 870. Advantage Shuffle.cpp
+++ 870. Advantage Shuffle.cpp
@@ -1,34 +1,15 @@
-int[] advantageCount(int[] nums1, int[] nums2) {
-    int n = nums1.length;
-    // 给 nums2 降序排序
-    PriorityQueue<int[]> maxpq = new PriorityQueue<>(
-        (int[] pair1, int[] pair2) -> { 
-            return pair2[1] - pair1[1];
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+        multiset<int> s;
+        for (int i:nums1) s.insert(i);
+        vector<int> ans(nums1.size(),0);
+        for (int i=0;i<nums2.size();i++){
+            auto it = s.upper_bound(nums2[i]);
+            if (it == s.end()) it = s.begin();
+            ans[i] = *it;
+            s.erase(it);
         }
-    );
-    for (int i = 0; i < n; i++) {
-        maxpq.offer(new int[]{i, nums2[i]});
+        return ans;
     }
-    // 给 nums1 升序排序
-    Arrays.sort(nums1);
-
-    // nums1[left] 是最小值，nums1[right] 是最大值
-    int left = 0, right = n - 1;
-    int[] res = new int[n];
-
-    while (!maxpq.isEmpty()) {
-        int[] pair = maxpq.poll();
-        // maxval 是 nums2 中的最大值，i 是对应索引
-        int i = pair[0], maxval = pair[1];
-        if (maxval < nums1[right]) {
-            // 如果 nums1[right] 能胜过 maxval，那就自己上
-            res[i] = nums1[right];
-            right--;
-        } else {
-            // 否则用最小值混一下，养精蓄锐
-            res[i] = nums1[left];
-            left++;
-        }
-    }
-    return res;
-}
+};
