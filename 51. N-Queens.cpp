53797a6ea3b072eac1bcd5e69b231f77714b77bb
yiheng666 <yiheng.gml@gmail.com>
2021-08-30 22:14:27 +0900

    no message

--- 51. N-Queens.cpp
+++ 51. N-Queens.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    vector<string> board;
-    bool col[20],gl[20],ugl[20];
-    void dfs(int row,int n){
-        //end condition
-        if (row == n){
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n,string(n,'.'));
+        dfs(board,0);
+        return ans;
+    }
+    void dfs(vector<string>& board,int row){
+        if (board.size() == row){
             ans.push_back(board);
             return;
         }
-        for (int i=0;i<n;i++){
-            if (!col[i] && !gl[row+i] && !ugl[n-row+i]){
-                col[i] = gl[row+i] = ugl[n-row+i] = true;
-                board[row][i] = 'Q';
-                dfs(row+1,n);
-                board[row][i] = '.';
-                col[i] = gl[row+i] = ugl[n-row+i] = false;
-            }
+        for (int i=0;i<board.size();i++){
+            if (!isValid(board,row,i))
+                continue;
+            board[row][i] = 'Q';            
+            dfs(board,row+1);
+            board[row][i] = '.'; 
         }
     }
-    vector<vector<string>> solveNQueens(int n) {
-        board.resize(n,string(n, '.'));
-        dfs(0,n);
-        return ans;
+    bool isValid(vector<string>& board,int row,int col){
+        for (int i=0;i<row;i++)
+            if (board[i][col] == 'Q') return false;
+        for (int i=row-1,j=col+1;i>=0&&j<board.size();i--,j++)
+            if (board[i][j] == 'Q') return false;
+        for (int i=row-1,j=col-1;i>=0&&j>=0;i--,j--)
+            if (board[i][j] == 'Q') return false;
+        return true;
     }
 };
