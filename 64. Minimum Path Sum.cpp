862c5011d530441c371f70c26e3116ab83d4b09a
yiheng666 <yiheng.gml@gmail.com>
2021-09-22 21:19:17 +0900

    no message

---
+++ 64. Minimum Path Sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(),n = grid[0].size();
+        vector<vector<int>> dp = grid;
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                if (i!=0 && j!=0) dp[i][j] += min(dp[i-1][j],dp[i][j-1]);
+                else if (i == 0 && j!=0) dp[i][j] += dp[i][j-1];
+                else if (i != 0 && j==0) dp[i][j] += dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
