62141444cbdfe29429afadbc0745d9d973a37646
yiheng666 <yiheng.gml@gmail.com>
2021-10-24 18:32:34 +0900

    no message

--- 211. Design Add and Search Words Data Structure.cpp
+++ 211. Design Add and Search Words Data Structure.cpp
@@ -1,33 +1,29 @@
-
-/*case全过 但是超时了
 class WordDictionary {
 public:
-    vector<string> v;
+    unordered_map<int,set<string>> m;
     WordDictionary() {
-        v.resize(0);
+        
     }
     
     void addWord(string word) {
-        v.push_back(word);
+        m[word.size()].insert(word);
     }
     
     bool search(string word) {
-        for (int i=0;i<v.size();i++){
-            if (v[i].size() == word.size()){
-                for (int j=0;j<word.size();j++){
-                    if (v[i][j] != word[j] &&  word[j] != '.'){
-                        break;
-                    }
-                    if (j == word.size()-1){
-                        return true;
-                    }
+        for (string s : m[word.size()]){
+            for (int j=0;j<word.size();j++){
+                if (s[j] != word[j] &&  word[j] != '.'){
+                    break;
+                }
+                if (j == word.size()-1){
+                    return true;
                 }
             }
         }
         return false;
     }
 };
-*/
+
 /**
  * Your WordDictionary object will be instantiated and called as such:
  * WordDictionary* obj = new WordDictionary();
