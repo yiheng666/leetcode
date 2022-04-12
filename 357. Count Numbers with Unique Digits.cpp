8aa0abf1ffeb482a3aa799e8422d861c8f79e3ed
yiheng666 <yiheng.gml@gmail.com>
2022-04-12 22:59:29 +0900

    no message

--- 357. Count Numbers with Unique Digits.cpp
+++ 357. Count Numbers with Unique Digits.cpp
@@ -1,21 +1,11 @@
 class Solution {
-public:
-    int countNumbersWithUniqueDigits(int n) {
-        if (n == 0) return 1;
-        vector<int> dp(n+1);
-        dp[1] = 10;
-        for (int i=2;i<=n;i++){
-            int a = 9,b = 9;
-            for (int j=i-1;j<n;j++){
-                a = a * b;
-                b--;
-            }
-            dp[i] = a;
-        }
-        int res = 0;
-        for (int i=0;i<=n;i++){
-            res += dp[i];
+    public int countNumbersWithUniqueDigits(int n) {
+        int res = 1;
+        int product = 9;
+        for (int i = 1; i < 10 && i <= n; i++) {
+            res = product + res;
+            product *= (10-i);
         }
         return res;
     }
-};
+}
